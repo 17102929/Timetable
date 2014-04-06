f0fbf029965fc80e2e55a8c9366d5ff9205d7735
Sora Mizuno <sora@Sora-no-MacBook-Air.local>
2014-04-06 08:12:14 +0800

    Initial Commit

---
+++ zikanwari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zikanwari
+//
+//  Created by Sora Mizuno on 4/6/14.
+//  Copyright (c) 2014 Sora Mizuno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zikanwari/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  zikanwari
+//
+//  Created by Sora Mizuno on 4/6/14.
+//  Copyright (c) 2014 Sora Mizuno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
